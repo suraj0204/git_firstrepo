10437976d32e6ef42f502d6b58cb618d5d77cf1e
suraj pal <surajpal0204@gmail.com>
2020-05-20 13:50:49 +0530

    if-else file

---
+++ if_else.py
@@ -0,0 +1,32 @@
+'''age=int(input("enter your age"))
+if age>=18:
+    print("you can vote sir")
+else:
+     print(" you cannot vote")
+print("this will always printed") '''
+
+'''#print("hello")
+for i in range(10):
+    print(i)'''
+'''for i in range(1,11):
+    print(3, 'x', i, '=' ,3*i)'''
+'''n=5
+temp=n
+fac=1
+while n>0:
+    fac=fac*n
+    n-=1
+print("factorail of ",temp,'=',fac) '''
+
+for i in range(10):
+    if(i==5):
+        pass
+    print(i)
+
+
+
+    
+
+
+
+    
